095ab070a65f76ed01e3d0f7e87bcd324159dd4f
Antonio Carrilho <antoniocarrilho3@gmail.com>
2026-02-06 19:36:18 -0300

    desafio imc

---
+++ desafio-imc.py
@@ -0,0 +1,13 @@
+imc = 25
+if imc < 18.5:
+    print("abaixo do peso")
+elif imc >= 18.5 and 24.9:
+    print("normal")
+elif imc >= 25.0 and 29.9:
+    print("sobrepeso")
+elif imc >= 30.0 and 34.9:
+    print("obesidade de grau I")
+elif imc >= 35.0 and 39.9:
+    print("obesidade de grau II")
+else:
+    print("obesidade de grau III")
---
+++ desafio.py
@@ -0,0 +1,7 @@
+# Executar-Tabuada de 7
+
+input("tabuada de 7")
+
+while contador <= 10:
+    
+    print 7 x 1  até 10
