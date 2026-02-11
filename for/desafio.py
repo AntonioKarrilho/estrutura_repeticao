8108fdc76aa28c3d20992147cced248e3dffec16
Antonio Carrilho <antoniocarrilho3@gmail.com>
2026-02-11 19:32:59 -0300

    desafio for

---
+++ for/desafio.py
@@ -0,0 +1,8 @@
+notas = [8, 5, 9, 6, 7, 3, 10]
+    
+for nota in notas:
+    if nota >= 7:
+        print(f"Nota(nota) - funcionário aprovado")
+
+    else:
+        print(f"Nota(nota) - funcionário em acompanhamento")    
